c37eea2cc6e61e2aff3d9d2a4b175bff68e39ed1
unknown <yves.randriamamory@vitsika.tech>
2023-10-13 11:55:59 +0200

    init tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LibModule } from './lib/lib.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    LibModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/lib/lib.module.ts
@@ -0,0 +1,114 @@
+import { NgModule } from '@angular/core';
+
+// MATERIAL
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+
+//OTHERS
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [
+
+    // Angular Material
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatCardModule,
+    MatIconModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatTabsModule,
+    MatInputModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatSortModule,
+    MatStepperModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatCardModule,
+    MatIconModule,
+    MatSelectModule,
+    MatRippleModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatTabsModule,
+    MatInputModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatSortModule,
+    MatStepperModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+})
+export class LibModule { }
