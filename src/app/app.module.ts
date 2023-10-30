f8de3540bff2f91d2df8d59fda07c729bfb45f41
unknown <yves.randriamamory@vitsika.tech>
2023-10-30 11:14:58 +0100

    pending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
 import { PlayComponent } from './play/play.component';
 import { HomeComponent } from './home/home.component';
 import { InfoModalComponent } from './info-modal/info-modal.component';
+import { PendingComponent } from './pending/pending.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { InfoModalComponent } from './info-modal/info-modal.component';
     ConfirmModalComponent,
     PlayComponent,
     HomeComponent,
-    InfoModalComponent
+    InfoModalComponent,
+    PendingComponent
   ],
   imports: [
     BrowserModule,
